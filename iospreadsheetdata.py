2cb5967c3d51057ee9f3c05da229a36f91dcda27
ImplyingICheck <amhector@stanford.edu>
2020-02-18 05:57:56 -0800

    Added verbose option to delete_all_files.

--- iospreadsheetdata.py
+++ iospreadsheetdata.py
@@ -1,19 +1,26 @@
 import pygsheets
+import secretsecret
 
 
 class IOSpreadsheetData:
     def __init__(self, authfile):
-        self.authfile = authfile
+        self.gc = pygsheets.authorize(service_account_file=authfile)
 
-    def delete_all_files(self):
-        gc = pygsheets.authorize(service_file=self.authfile)
-        for file in gc.open_all():
-            print("File Deleted: " + file.title)
+    def delete_all_files(self, verbose=False):
+        files = self.gc.open_all()
+        for file in files:
+            if verbose:
+                print("File Deleted: " + file.title)
             file.delete()
         print("All files deleted.")
 
     def print_all_file_names(self):
-        gc = pygsheets.authorize(service_file=self.authfile)
-        for file in gc.open_all():
+        files = self.gc.open_all()
+        for file in files:
             print("File found: " + file.title)
         print("All file names printed.")
+
+    def share_sheet(self, sheet_title, gmail):
+        sh = self.gc.open(sheet_title)
+        sh.share(gmail, role='writer', type='user')
+        print('File Shared')
